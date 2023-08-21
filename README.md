ba303fe19ec04129463d351876902d0540c9ec8e
Yassen Efremov <48584741+YassenEfremov@users.noreply.github.com>
2023-08-21 13:06:30 +0300

    Create initial flutter app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hedgehog - the official SONICC software
+
+Hedgehog allows you to monitor and command your SONICC system!
---
+++ android/app/src/main/kotlin/com/example/hedgehog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hedgehog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
