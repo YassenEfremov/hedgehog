49a819bf10c1ec3d64e99185b7818f12c03d2f70
Yassen Efremov <48584741+YassenEfremov@users.noreply.github.com>
2023-08-22 14:03:04 +0300

    Create app layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,252 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+// import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:bluez/bluez.dart';
 
-void main() {
-  runApp(const MyApp());
+
+void main() async {
+
+  // var client = BlueZClient();
+  // await client.connect();
+
+  // if (client.adapters.isEmpty) {
+  //   print('No Bluetooth adapters found');
+  //   await client.close();
+  //   return;
+  // }
+  // var adapter = client.adapters[0];
+
+  // print('Searching for devices on ${adapter.name}...');
+  // for (var device in client.devices) {
+  //   print('  ${device.address} ${device.name}');
+  // }
+  // client.deviceAdded
+  //     .listen((device) => print('  ${device.address} ${device.name}'));
+
+  // await adapter.startDiscovery();
+
+  // await Future.delayed(Duration(seconds: 5));
+
+  // await adapter.stopDiscovery();
+
+  // await client.close();
+
+  runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Hedgehog',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue.shade900),
+        ),
+        home: MyHomePage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+
+class MyAppState extends ChangeNotifier {
+  // var current = WordPair.random();
+
+  // void getNext() {
+  //   current = WordPair.random();
+  //   notifyListeners();
+  // }
+
+  // var favorites = <WordPair>[];
+
+  // void toggleFavorite() {
+  //   if (favorites.contains(current)) {
+  //     favorites.remove(current);
+  //   } else {
+  //     favorites.add(current);
+  //   }
+  //   notifyListeners();
+  // }
+}
+
+
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MyHomePageState extends State<MyHomePage> {
+
+  var selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = TelemetryPage();
+        break;
+      case 1:
+        page = ControlPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.monitor_heart),
+                      label: Text('Telemetry'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.gamepad),
+                      label: Text('Control'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
     );
+  }
+}
+
+
+class TelemetryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Center(
+      child: GridView.count(
+        shrinkWrap: true,
+        crossAxisCount: 2,
+        children: [
+          Angle(),
+          RPM()
+        ],
+      ),
+    );
+  }
+}
+
+
+class Angle extends StatefulWidget {
+  @override
+  State<Angle> createState() => _AngleState();
+}
+
+class _AngleState extends State<Angle> {
+
+  var angle = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final titleStyle = theme.textTheme.displaySmall!.copyWith(
+      color: theme.colorScheme.onBackground,
+    );
+    final angleStyle = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onBackground,
+      fontWeight: FontWeight.bold
+    );
+
+    return Card(
+      color: theme.colorScheme.background,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Angle',
+            style: titleStyle,
+          ),
+          Text(
+            '$angle deg',
+            style: angleStyle,
+          ),
+        ],
+      )
+    );
+  }
+}
+
+
+class RPM extends StatefulWidget {
+  @override
+  State<RPM> createState() => _RPMState();
+}
+
+class _RPMState extends State<RPM> {
+
+  var rpm = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final titleStyle = theme.textTheme.displaySmall!.copyWith(
+      color: theme.colorScheme.onBackground,
+    );
+    final RPMStyle = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onBackground,
+      fontWeight: FontWeight.bold
+    );
+
+    return Card(
+      color: theme.colorScheme.background,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Motor RPM',
+            style: titleStyle,
+          ),
+          Text(
+            '$rpm rpm',
+            style: RPMStyle,
+          ),
+        ],
+      )
+    );
+  }
+}
+
+
+class ControlPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    final theme = Theme.of(context);
+
+    return Placeholder();
   }
 }
