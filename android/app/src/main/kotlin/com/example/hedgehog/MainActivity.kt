566e6b46bfd99207e91b163170a2613183ceb71d
Yassen Efremov <48584741+YassenEfremov@users.noreply.github.com>
2023-08-25 17:50:02 +0300

    Replace app with bluetooth serial example

--- android/app/src/main/kotlin/com/example/hedgehog/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hedgehog
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'home_page/home_page.dart';
-import 'telemetry_page/telemetry_page.dart';
-import 'control_page/control_page.dart';
+import './MainPage.dart';
 
+void main() => runApp(new ExampleApplication());
 
-void main() {
-  runApp(Hedgehog());
-}
-
-
-class Hedgehog extends StatelessWidget {
-  const Hedgehog({super.key});
-
+class ExampleApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => AppState(),
-      child: MaterialApp(
-        title: 'Hedgehog',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue.shade900),
-        ),
-        home: MainPage(),
-      ),
-    );
-  }
-}
-
-
-class AppState extends ChangeNotifier {
-
-}
-
-
-class MainPage extends StatefulWidget {
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-
-  var selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = HomePage();
-        break;
-      case 1:
-        page = TelemetryPage();
-        break;
-      case 2:
-        page = ControlPage();
-        break;
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
-
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          body: Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: constraints.maxWidth >= 600,
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.monitor_heart),
-                      label: Text('Telemetry'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.gamepad),
-                      label: Text('Control'),
-                    ),
-                  ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) {
-                    setState(() {
-                      selectedIndex = value;
-                    });
-                  },
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
-                ),
-              ),
-            ],
-          ),
-        );
-      }
-    );
+    return MaterialApp(home: MainPage());
   }
 }
